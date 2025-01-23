3accb6846436b9259da04156479e0ed5f3534dfd
supreethsk <supreeth1997@gmail.com>
2025-01-23 17:38:01 +0530

    resolve comit 3

--- src/new.js
+++ src/new.js
@@ -4,6 +4,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { CiSearch } from "react-icons/ci";
 import "../src/App.css";
+import { jsPDF } from "jspdf";
+// import Bgimg from "../src/assets/banner.jpg"
+import Image from '../src/assets/image.jpeg'
 
 const GoogleSheetPNRFetcher = () => {
   const [pnrInput, setPnrInput] = useState('');
@@ -61,6 +64,29 @@ const GoogleSheetPNRFetcher = () => {
     } finally {
       setLoading(false);
     }
+  };
+
+  const saveAsPDF = (data) => {
+    const doc = new jsPDF();
+    doc.setFont("helvetica");
+
+    // Add Title
+    doc.text("PNR Details", 10, 10);
+
+    // Add PNR Data to PDF
+    doc.text(`First Name: ${data.First_name}`, 10, 20);
+    doc.text(`Last Name: ${data.Last_name}`, 10, 30);
+    doc.text(`Passport Number: ${data.Passport_number}`, 10, 40);
+    doc.text(`Address: ${data.Address}`, 10, 50);
+    doc.text(`Date of Vaccine: ${data.Date_of_vaccine}`, 10, 60);
+    doc.text(`Valid Until: ${data.Valid_until}`, 10, 70);
+    doc.text(`Details of Vaccine: ${data.Details_of_vaccine}`, 10, 80);
+    doc.text(`Hospital Address: ${data.Hospital_address}`, 10, 90);
+    doc.text(`Contact Number: ${data.Contact_number}`, 10, 100);
+    doc.text(`PNR: ${data.Pnr}`, 10, 110);
+
+    // Save the PDF
+    doc.save(`PNR_${data.Pnr}.pdf`);
   };
 
   return (
@@ -136,7 +162,15 @@ const GoogleSheetPNRFetcher = () => {
     </ul>
   </div>
 </nav>
-<div className='bgImage pt-5'>
+<div>
+  <img src={Image} alt="" style={{width: "100%"}} />
+</div>
+<div className='pt-5 pb-5' style={{backgroundColor: "#248cbf"}}>
+  <center>
+  <h5>
+    CHECK PNR NOW !!!
+  </h5>
+  </center>
       <div class="form-group form-search mb-3">
         <input 
         class="form-control form-control-search" 
@@ -154,7 +188,7 @@ const GoogleSheetPNRFetcher = () => {
     {pnrData && pnrData.length > 0 ? (
         <div className='containers'>
           {pnrData.map((data, index) => (
-            <div className='row mb-3 divContainer' key={index}>
+            <div className='row m-0 mb-3 divContainer' key={index}>
                 <div className='col-4 col-md-3 col-lg-3 text-break text-center p-3'>
                 First Name
                 <div>
@@ -222,6 +256,12 @@ const GoogleSheetPNRFetcher = () => {
                 <strong>{data.Pnr}</strong>
                 </div>
                 </div>
+                <button
+                  onClick={() => saveAsPDF(data)}
+                  className="btn btn-black mt-2"
+                >
+                  Save as PDF
+                </button>
               {/* <strong>Date of Vaccine:</strong> {data.Date_of_vaccine} <br />
               <strong>Valid Until:</strong> {data.Valid_until} <br />
               <strong>Details of Vaccine:</strong> {data.Details_of_vaccine} <br />
@@ -235,7 +275,7 @@ const GoogleSheetPNRFetcher = () => {
         <>
         <div className='row m-0 justify-content-center'>No data found or enter a valid PNR.</div>
         <div className='row m-0 justify-content-center'>
-          <div className='col-12 col-md-5 mt-5 word-wrap'>
+          <div className='col-12 col-md-5 mt-5 word-wrap text-center'>
         <h5>Welcome to the COVID Vaccine PNR Status Check page! Here, you can easily track the status of your COVID-19 vaccination by checking your PNR (Personal Number Reference). Our user-friendly tool provides you with real-time updates
         </h5>
           </div>
